7e4d2540bba1e9437d41d9c967901c40d1168ff1
caramnix <35668367+caramnix@users.noreply.github.com>
2022-10-10 18:17:36 -0400

    Update H116_cospo_viz.py
    update repo name

--- H116_cospo_viz.py
+++ H116_cospo_viz.py
@@ -9,7 +9,7 @@ import streamlit as st
 st.markdown("<div style='background:#e6e6e6'><h3 style='font-weight:bold; color:#252626'>  Cosponsorship Network, 116th Congress (2019-2021)</h3></div>", unsafe_allow_html=True)
 
 
-url = 'https://github.com/caramnix/Social-Influence/blob/main/adj_matrix_116_House_Fowler.xlsx?raw=true'
+url = 'https://github.com/caramnix/Social-Influence-Viz/blob/main/adj_matrix_116_House_Fowler.xlsx?raw=true'
 df = pd.read_excel(url)
 
 labels_dict= df.iloc[:,0].to_dict()
@@ -24,7 +24,7 @@ H = nx.relabel_nodes(G, labels_dict)
 H.remove_edges_from([(n1, n2) for n1, n2, w in H.edges(data="weight") if w < 3])
 
 
-url2= 'https://github.com/caramnix/Social-Influence/raw/main/leginfo_116H.csv'
+url2= 'https://github.com/caramnix/Social-Influence-Viz/raw/main/leginfo_116H.csv'
 leg_info= pd.read_csv(url2) #"/Users/caranix/Library/CloudStorage/OneDrive-TheOhioStateUniversity/Social Influence/Data/Congress/116/House/leginfo_116H.csv")
 
 leg_info= leg_info.rename(index= leg_info.iloc[:,0].to_dict()) 
@@ -35,7 +35,7 @@ leg_dict= leg_info.to_dict('index')
 
 nx.set_node_attributes(H, leg_dict)
 
-url3 = 'https://raw.githubusercontent.com/caramnix/Social-Influence/main/connectedness_house_116.csv'
+url3 = 'https://raw.githubusercontent.com/caramnix/Social-Influence-Viz/main/connectedness_house_116.csv'
 connectedness_leg= pd.read_csv(url3)
 
 connectedness_leg= connectedness_leg.rename(index= connectedness_leg.iloc[:,0].to_dict()) 
